1a4058f0f01b0ae9f30dbdc55357bb7193332f41
Mikael Biström <info@studiotriangular.se>
2017-10-14 18:38:50 +0200

    Adding testing utilities file

---
+++ tests/utilities/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\User;
+
+if (!function_exists('fsltCreateUser')) {
+    function fsltCreateUser($vars = [])
+    {
+        $password = 'secret';
+        $name = 'Tester Person '.rand(10000, 99999);
+        $email = 
+        $defaults = [
+            'name' => $name,
+            'password' => $password,
+            'email' => snake_case($name)."@testingemail.se"
+        ];
+        
+        $properties = array_merge($defaults, $vars);
+        $properties['password'] = bcrypt($properties['password']);
+
+        $user = new User();
+        $user->fill($properties);
+        $user->save();
+        return $user;
+    }
+
+}
