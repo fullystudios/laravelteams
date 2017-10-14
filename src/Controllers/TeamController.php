d15bd954f2b6bc2900f0b8dba00b74fdac4b0668
Mikael Biström <info@studiotriangular.se>
2017-10-14 13:05:34 +0200

    Fixing route to controller connection

--- src/Controllers/TeamController.php
+++ src/Controllers/TeamController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace FullyStudios\LaravelTeams\Controllers;
 
+use App\Http\Controllers\Controller;
 use App\Team;
 use Illuminate\Http\Request;
 
--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,11 @@
 <?php
-Route::get('teams', function(){
-    echo 'Hello from teams';
+
+Route::group(['namespace' => 'FullyStudios\LaravelTeams\Controllers'], function()
+{
+    Route::get('/teams', 'TeamController@index')->name('teams.index');
+    Route::get('/teams/create', 'TeamController@create')->name('teams.create');
+    Route::get('/teams/{product}', 'TeamController@show')->name('teams.show');
+    Route::post('/teams', 'TeamController@store')->name('teams.store');
+    Route::delete('/teams/{product}', 'TeamController@destroy')->name('teams.delete');
+
 });
