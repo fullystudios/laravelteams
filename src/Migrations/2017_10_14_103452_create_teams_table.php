0faa27ff3ec28bc92129f50bef8bec2c90798b9c
Mikael Biström <info@studiotriangular.se>
2017-10-15 17:50:13 +0200

    Fixing spacing

--- src/Migrations/2017_10_14_103452_create_teams_table.php
+++ src/Migrations/2017_10_14_103452_create_teams_table.php
@@ -18,6 +18,7 @@ class CreateTeamsTable extends Migration
             $table->string('name');
             $table->integer('owner_id')->unsigned()->nullable();
             $table->timestamps();
+            
             $table->foreign('owner_id')->references('id')->on('users');
         });
 
